61a18de4748410b903d3dff911bc63858c10fa61
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2025-10-11 01:55:17 +0530

    Refactor code structure for improved readability and maintainability

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "ResumeFEED" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track You Resume Application</h1>
+        <h2>Review you resume and get AI-powred Feedback</h2>
+      </div>
+    </section>
+    
+  </main>
 }
